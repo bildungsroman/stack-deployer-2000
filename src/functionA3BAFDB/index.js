6717df8a07c61bbde8e8edb1d845cb1db8cab854
Anna Spysz <anna.spysz@stackery.io>
2018-12-17 15:37:31 -0800

    testing tester

--- src/functionA3BAFDB/index.js
+++ src/functionA3BAFDB/index.js
@@ -1,5 +1,15 @@
-exports.handler = async message => {
-  console.log(message);
+const child_process = require('child_process');
 
-  return {};
+exports.handler = function(event, context) {
+  console.log('Doing things');
+  const child = child_process.spawn('/bin/bash', [ '-c', 'curl --silent --output - --max-time 1 https://9y8ud8ckk4.execute-api.us-west-2.amazonaws.com/development/test || true' ], { stdio: 'inherit' });
+  console.log(child);
+
+  child.on('close', function(code) {
+    if(code !== 0) {
+      return context.done(new Error("non zero process exit code"));
+    }
+
+    context.done(null);
+  });
 }
