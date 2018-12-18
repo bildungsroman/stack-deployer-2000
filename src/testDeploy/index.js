ccc68b4f9bc2a0e3e11ff43d96b07eca3accb807
Anna Spysz <anna.spysz@stackery.io>
2018-12-17 16:53:05 -0800

    shell experiments

--- src/testDeploy/index.js
+++ src/testDeploy/index.js
@@ -1,14 +1,13 @@
 exports.handler = function(event, context, callback) {
   const exec = require('child_process').exec;
-
-  exec('echo $PWD && ls -l && env && cd /tmp && echo $PWD', (error, stdout, stderr) => {
+  console.log("Doing things");
+  
+  exec('echo $PWD && ls -l && env && cd /tmp && echo $PWD && export HOME="/tmp" && npm install *** --prefix="/tmp"', (error, stdout, stderr) => {
     if (error) {
-      console.log("Error occurs");
-      console.error(error);
+      console.error(`exec error: ${error}`);
     }
-    console.log("Doing things");
-    console.log(stdout);
-    console.log(stderr);
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
   });
   console.log("Did things");
 }
