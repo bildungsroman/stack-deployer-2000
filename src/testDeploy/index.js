8158635472a30cfa2bfa7070470de414ffa98b3c
Anna Spysz <anna.spysz@stackery.io>
2018-12-17 16:32:15 -0800

    trying things

--- src/testDeploy/index.js
+++ src/testDeploy/index.js
@@ -1,11 +1,10 @@
 exports.handler = function(event, context, callback) {
   const exec = require('child_process').exec;
 
-  exec('echo $PWD && ls -l && env && npm install aws-sdk && aws', (error, stdout, stderr) => {
+  exec('echo $PWD && ls -l && env && cd /tmp && npm install aws-sdk && aws', (error, stdout, stderr) => {
     if (error) {
       console.log("Error occurs");
       console.error(error);
-      return;
     }
     console.log("Doing things");
     console.log(stdout);
