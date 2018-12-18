a991989197ccd1343e864a9c861e214931915ea6
Anna Spysz <anna.spysz@stackery.io>
2018-12-18 13:18:32 -0800

    testing

--- src/handleDeploy/index.js
+++ src/handleDeploy/index.js
@@ -3,21 +3,18 @@ process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'
 exports.handler = function(event, context, callback) {
   const AWS = require('aws-sdk');
   // use spawnSync to execute the bash process
-  const spawnSync = require('child_process').spawnSync;
+  const spawn = require('child_process').spawnSync;
   // get the GitHub secret from the environment variables
   const token = process.env.GITHUB_WEBHOOK_SECRET;
   // get the remaining variables from the GitHub event
   const headers = event.headers;
   const githubEvent = headers['X-GitHub-Event'];
   const body = JSON.parse(event.body);
-  // this prevents errors from the GitHub ping event
-  const username = body.pusher ? body.pusher.name : body.repository.owner.login;
-  
   const { repository } = body;
   const repo = repository.name;
   const url = repository.html_url;
   
-
+  // check for secret
   if (typeof token !== 'string') {
     let errMsg = 'Must provide a \'GITHUB_WEBHOOK_SECRET\' env variable';
     return callback(null, {
@@ -36,7 +33,7 @@ exports.handler = function(event, context, callback) {
   console.log(`This is the repo url we'll be using to deploy: ${url}.`);
 
   // spawnSync('./deploy.sh', (error, stdout, stderr) => {
-  spawnSync('echo $PWD && ls -l && env && cd /tmp && echo $PWD ', (error, stdout, stderr) => {
+  spawn('echo $PWD && ls -l && env && cd /tmp && echo $PWD', [], (error, stdout, stderr) => {
     if (error) {
       console.error(`spawnSync error: ${error}`);
       callback(error);
